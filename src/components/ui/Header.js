20155ce697d22cb7b2810ae454b4f6a73e6d565e
jaywomack <womackjw@hotmail.com>
2021-01-10 19:37:21 -0800

    header complete. Creating menu.

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+import { makeStyles } from '@material-ui/styles';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Button from '@material-ui/core/Button';
+import { Link } from 'react-router-dom';
+
+import logo from '../../assets/logo.svg';
 
 function ElevationScroll(props) {
   const { children } = props;
@@ -15,12 +22,122 @@ function ElevationScroll(props) {
   });
 }
 
+const useStyles = makeStyles((theme) => ({
+  toolbarMargin: {
+    ...theme.mixins.toolbar,
+    marginBottom: '3em',
+  },
+  logo: {
+    height: '8em',
+  },
+  logoContainer: {
+    padding: 0,
+    '&:hover': {
+      backgroundColor: 'transparent',
+    },
+  },
+  tabContainer: {
+    marginLeft: 'auto',
+  },
+  tab: {
+    ...theme.typography.tab,
+    minWidth: 10,
+    marginLeft: '25px',
+  },
+  button: {
+    ...theme.typography.estimate,
+    borderRadius: '50px',
+    marginLeft: '50px',
+    marginRight: '25px',
+    height: '45px',
+  },
+}));
+
 export default function Header(props) {
+  const classes = useStyles();
+  const [value, setValue] = useState(0);
+
+  const handleChange = (e, value) => {
+    setValue(value);
+  };
+
+  useEffect(() => {
+    if (window.location.pathname === '/' && value !== 0) {
+      setValue(0);
+    } else if (window.location.pathname === '/services' && value !== 1) {
+      setValue(1);
+    } else if (window.location.pathname === '/revolution' && value !== 2) {
+      setValue(2);
+    } else if (window.location.pathname === '/about' && value !== 3) {
+      setValue(3);
+    } else if (window.location.pathname === '/contact' && value !== 4) {
+      setValue(4);
+    } else if (window.location.pathname === '/estimate' && value !== 5) {
+      setValue(5);
+    }
+  }, [value]);
   return (
-    <ElevationScroll>
-      <AppBar position='fixed' color='primary'>
-        <Toolbar>Arc Development</Toolbar>
-      </AppBar>
-    </ElevationScroll>
+    <React.Fragment>
+      <ElevationScroll>
+        <AppBar position='fixed' color='primary'>
+          <Toolbar disableGutters>
+            <Button
+              component={Link}
+              to='/'
+              disableRipple
+              onClick={() => setValue(0)}
+              className={classes.logoContainer}
+            >
+              <img src={logo} className={classes.logo} alt='Company logo' />
+            </Button>
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              className={classes.tabContainer}
+              indicatorColor='primary'
+            >
+              <Tab
+                className={classes.tab}
+                component={Link}
+                label='Home'
+                to='/'
+              />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                label='Services'
+                to='/services'
+              />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                label='The Revolution'
+                to='/revolution'
+              />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                label='About Us'
+                to='/about'
+              />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                label='Contact Us'
+                to='/contact'
+              />
+            </Tabs>
+            <Button
+              variant='contained'
+              color='secondary'
+              className={classes.button}
+            >
+              Free Estimate
+            </Button>
+          </Toolbar>
+        </AppBar>
+      </ElevationScroll>
+      <div className={classes.toolbarMargin} />
+    </React.Fragment>
   );
 }
